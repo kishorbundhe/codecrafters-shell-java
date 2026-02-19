16b0931956f7b45dcddfadcbbfd2983e2d6e35ab
Kishor Bundhe <kishorbundhe@gmail.com>
2026-02-19 23:13:54 +0100

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        for(;;) {
+            getCommand();
+        }
+    }
+
+    private static void getCommand() {
         System.out.print("$ ");
         Scanner scanner = new Scanner(System.in);
         String command = scanner.nextLine();
         System.out.println(command + ": command not found");
-        
     }
 }
