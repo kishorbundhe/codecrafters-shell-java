115c59932d8d0df11a900d5badf130ec8f8039f3
Kishor Bundhe <kishorbundhe@gmail.com>
2026-02-19 23:23:51 +0100

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        for(;;) {
+        for (;;) {
             if (!shouldContinueRunningCommand()) {
                 break;
             }
@@ -13,8 +13,14 @@ public class Main {
         System.out.print("$ ");
         Scanner scanner = new Scanner(System.in);
         String command = scanner.nextLine();
-        if(command.equals("exit")) {
-            return false;
+        switch (command) {
+            case "exit":
+                return false;
+            case "echo":
+                System.out.println("echo " + command.substring(5));
+                break;
+            default:
+                break;
         }
         System.out.println(command + ": command not found");
         return true;
