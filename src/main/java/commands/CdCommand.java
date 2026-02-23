20787528e8251b1c7ead322a0fcd4725313687ea
Kishor Bundhe <kishorbundhe@gmail.com>
2026-02-24 00:11:31 +0100

    codecrafters submit [skip ci]

--- src/main/java/commands/CdCommand.java
+++ src/main/java/commands/CdCommand.java
@@ -7,20 +7,29 @@ public class CdCommand implements Command {
 
     @Override
     public boolean execute(String command, String options) {
-        Path newPath = Path.of(options);
+        Path newPath;
+        if (options.equalsIgnoreCase("~")) {
+            newPath = Path.of(System.getProperty("user.home"));
+            cdToPath(null, newPath);
+            return true;
+        }
+        newPath = Path.of(options);
         if (newPath.isAbsolute()) {
-            newPath = newPath.normalize();
+            newPath = newPath.resolve(options).normalize();
         } else {
             Path path = Path.of(System.getProperty("user.dir")).resolve(newPath);
             newPath = path.normalize();
         }
+        cdToPath(options, newPath);
+        return true;
+    }
+
+    private void cdToPath(String options, Path newPath) {
         if (Files.exists(newPath) && Files.isDirectory(newPath)) {
             System.setProperty("user.dir", newPath.toString());
         } else {
             System.out.println("cd: " + options + ": No such file or directory");
         }
-
-        return true;
     }
 
 }
