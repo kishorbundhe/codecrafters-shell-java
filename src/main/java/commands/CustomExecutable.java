fcb0c2e5da009b9e5190ae9c4370cc61eb976b3b
Kishor Bundhe <kishorbundhe@gmail.com>
2026-02-26 00:53:09 +0100

    codecrafters submit [skip ci]

--- src/main/java/commands/CustomExecutable.java
+++ src/main/java/commands/CustomExecutable.java
@@ -1,8 +1,11 @@
 package commands;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -11,9 +14,19 @@ public class CustomExecutable implements Command {
     public boolean execute(String command, String options) {
         List<String> args;
         if (command.contains("cat")) {
+            Pattern pattern = Pattern.compile("'([^']*)'");
+            String copyOptions = options;
+            Matcher matcher = pattern.matcher(copyOptions);
+            ArrayList<String> files = new ArrayList<>();
+            while (matcher.find()) {
+                String toReplace = matcher.group(0); 
+                // group 0 =. 'world hello' group 1 = world hello
+                files.add(toReplace);
+                copyOptions = copyOptions.replaceFirst("'([^']*)'", "$1");
+            }
             args = Stream.concat(
                     Stream.of(command),
-                    Stream.of(options))
+                    files.stream())
                     .collect(Collectors.toList());
         } else {
             args = Stream.concat(
