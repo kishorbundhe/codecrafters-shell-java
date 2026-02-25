30ac90a5655dfa74871eaabbea33c544071a36d2
Kishor Bundhe <kishorbundhe@gmail.com>
2026-02-25 23:55:07 +0100

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,13 +3,15 @@ import static commands.Command.commandNotFound;
 
 import java.nio.file.Path;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import commands.CdCommand;
 import commands.CustomExecutable;
 import commands.EchoComand;
 import commands.ExitCommand;
 import commands.Pair;
 import commands.PwdCommand;
-import commands.CdCommand;
 import commands.TypeCommand;
 import commands.ValidCommand;
 
@@ -32,7 +34,7 @@ public class Main {
         }
         String command = inputFromUser.split(" ")[0];
         String options = inputFromUser.replaceFirst(command, "").trim();
-        options = quoteOptionsIfNeeded(command, options);
+        options = escapeSingleQuotes(command, options);
         if (command.equals(ValidCommand.PWD.getCommand())) {
             return new PwdCommand().execute(command, options);
         } else if (command.equals(ValidCommand.CD.getCommand())) {
@@ -56,16 +58,23 @@ public class Main {
         return true;
     }
 
-    private static String quoteOptionsIfNeeded(String command, String options) {
+    // 'world hello' 'shell''script' example''test
+    private static String escapeSingleQuotes(String command, String options) {
 
-        if (options.startsWith("'") && options.endsWith("'")) {
-            options = options.replaceAll("^'|'$", "");
-        } else {
-            options = options.replaceAll("\\s+", " ");
-        
-        }
         options = options.replaceAll("\'\'", "");
+        Pattern pattern = Pattern.compile("'([^']*)'");
+        String copyOptions = options;
+        Matcher matcher = pattern.matcher(copyOptions);
+        while (matcher.find()) {
+            String toReplace = matcher.group(1); // group 0 =. 'world hello' group 1 = world hello
+            toReplace = toReplace.replaceAll("\\s+", "@"); // replace spaces with @
+            copyOptions = copyOptions.replaceFirst("'([^']*)'", toReplace);
+        }
+        options = copyOptions;
+        options = options.replaceAll("\\s+", " ");
+        options = options.replaceAll("@", " ");
         return options;
+      
     }
 
 }
