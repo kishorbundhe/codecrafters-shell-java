c6f5c0aac286e78405cf929b64f818e6d60d7c1f
Kishor Bundhe <kishorbundhe@gmail.com>
2026-03-26 00:17:30 +0100

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -94,7 +94,7 @@ public class Main {
                 matchedCandidates.forEach(str -> buffer.append(str).append("  "));
                 String str = buffer.toString().trim();
                 //lineReader.getTerminal().writer().println(str);
-                lineReader.getTerminal().writer().print("$ " + str);
+                lineReader.getTerminal().writer().print(str);
                 lineReader.getTerminal().flush();
                 lineReader.callWidget(LineReader.REDRAW_LINE);
                 lineReader.callWidget(LineReader.REDISPLAY);
