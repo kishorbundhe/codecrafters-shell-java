d8ba66eadbcfafb5761d90312a569303d935371c
Kishor Bundhe <kishorbundhe@gmail.com>
2026-04-06 23:27:19 +0200

    codecrafters submit [skip ci]

--- src/main/java/commands/EchoComand.java
+++ src/main/java/commands/EchoComand.java
@@ -3,7 +3,9 @@ package commands;
 public class EchoComand implements Command {
   @Override
   public boolean execute(UserInput userInput) {
-    Command.clearFiles(userInput.outputfile().getPath());
+    if (userInput.outputfile() != null) {
+      Command.clearFiles(userInput.outputfile().getPath());
+    }
     String options = userInput.options();
     // options = options.replaceAll("^\"|\"$", ""); // remove surrounding quotes if present
     System.out.println(options);
--- src/main/java/commands/TypeCommand.java
+++ src/main/java/commands/TypeCommand.java
@@ -5,7 +5,9 @@ import java.nio.file.Path;
 public class TypeCommand implements Command {
     @Override
     public boolean execute(UserInput userInput) {
-        Command.clearFiles(userInput.outputfile().getPath());
+        if (userInput.outputfile() != null) {
+            Command.clearFiles(userInput.outputfile().getPath());
+        }
         String options = userInput.options();
         // since command = type, we need to check if options is a valid command
         if (!ValidCommand.isValidCommand(options)) {
